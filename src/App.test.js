8f6e231c8a60ec6c6ae0ecc5db23aacee261e181
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-16 16:33:19 +0600

    chore: add an example test suite and an example function to render
    The describe defines a test suite where the first name is typically the name of the component or the function we are going to test. The test case described by the keyword 'it' perform certain checks with the help of assertive functions. It renders any valid JSX using the render function and also displays the HTML using the screen.debug()

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('true is truthy and false is falsy', () => {
+  it('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
+
+    screen.debug();
+  });
 });
