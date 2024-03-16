0fb3b557b81bf0c671f9e93b2af0da26cb4c3066
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-16 20:31:49 +0600

    feat: add user events for testing the usage

--- src/Async.test.js
+++ src/Async.test.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './Async';
 
@@ -27,5 +28,40 @@ describe('App', () => {
       });
   
       expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+    });
+});
+
+describe('App', () => {
+    it('renders App component', () => {
+      render(<App />);
+  
+      expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'JavaScript' },
+      });
+  
+      waitFor(() =>
+        expect(
+          screen.getByText(/Searches for JavaScript/)
+        ).toBeInTheDocument()
+      );
+    });
+});
+
+describe('App', () => {
+    it('renders App component', async () => {
+      render(<App />);
+  
+      // wait for the user to resolve
+      await screen.findByText(/Signed in as/);
+  
+      expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  
+      await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+  
+      expect(
+        screen.getByText(/Searches for JavaScript/)
+      ).toBeInTheDocument();
     });
 });
