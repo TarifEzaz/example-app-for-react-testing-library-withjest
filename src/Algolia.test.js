7d324299ddaadad54eee840c07c7f0c194d06c61
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-16 21:43:05 +0600

    chore: add test for mocking api

---
+++ src/Algolia.test.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import axios from 'axios';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from './Algolia';
+
+// Jest
+// jest.mock('axios');
+// Vitest
+jest.mock('axios');
+
+describe('Algolia', () => {
+  it('fetches stories from an API and displays them', async () => {
+    const stories = [
+      { objectID: '1', title: 'Hello' },
+      { objectID: '2', title: 'React' },
+    ];
+
+    axios.get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { hits: stories } })
+    );
+
+    render(<App />);
+
+    await userEvent.click(screen.getByRole('button'));
+
+    const items = await screen.findAllByRole('listitem');
+
+    expect(items).toHaveLength(2);
+  });
+});
