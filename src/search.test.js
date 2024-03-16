7399d74ce83763053668798ec6b8dd2e886ae49f
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-16 20:39:50 +0600

    chore: test component in isolation

--- src/search.test.js
+++ src/search.test.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import Search from './Search';
+import App, { Search } from './Search';
 
 describe('Search', () => {
   it('renders Search component', () => {
-    render(<Search />);
+    render(<App />);
 
     expect(screen.getByText('Search:')).toBeInTheDocument();
 
@@ -14,3 +15,43 @@ describe('Search', () => {
     expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
   });
 });
+
+describe('Search', () => {
+    it('calls the onChange callback handler', () => {
+      // Jest
+      // const onChange = jest.fn();
+      // Vitest
+      const onChange = jest.fn();
+  
+      render(
+        <Search value="" onChange={onChange}>
+          Search:
+        </Search>
+      );
+  
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'JavaScript' },
+      });
+  
+      expect(onChange).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('Search', () => {
+    it('calls the onChange callback handler', async () => {
+      // Jest
+      // const onChange = jest.fn();
+      // Vitest
+      const onChange = jest.fn();
+  
+      render(
+        <Search value="" onChange={onChange}>
+          Search:
+        </Search>
+      );
+  
+      await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+  
+      expect(onChange).toHaveBeenCalledTimes(10);
+    });
+  });
