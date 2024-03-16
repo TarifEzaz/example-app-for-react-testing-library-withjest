6963ce53809fc751739e157fade256d39b97e545
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-16 17:13:41 +0600

    chore: add event testing

--- src/Async.test.js
+++ src/Async.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './Async';
 
@@ -11,4 +11,21 @@ describe('Async', () => {
 
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
   });
+});
+
+describe('App', () => {
+    it('renders App component', async () => {
+      render(<App />);
+  
+      // wait for the user to resolve
+      await screen.findByText(/Signed in as/);
+  
+      expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'JavaScript' },
+      });
+  
+      expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+    });
 });
