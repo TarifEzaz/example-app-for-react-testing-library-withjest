c5fc6f20cf2ada29ae44b1d35692a104de555ae1
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-17 16:22:03 +0600

    chore: format code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 const title = 'Hello React';
 
 function App() {
-  return <div>{title}</div>;
+	return <div>{title}</div>;
 }
 
 export default App;
