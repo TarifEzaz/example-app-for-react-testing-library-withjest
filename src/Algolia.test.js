09a3a13a2872a7eb19cd6f0bf1cf252723f8d8e9
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-17 16:19:49 +0600

    chore: format code

--- src/Algolia.test.js
+++ src/Algolia.test.js
@@ -1,51 +1,67 @@
 import * as React from 'react';
 import axios from 'axios';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from './Algolia';
 
-// Jest
-// jest.mock('axios');
-// Vitest
 jest.mock('axios');
 
 describe('Algolia', () => {
-  it('fetches stories from an API and displays them', async () => {
-    const stories = [
-      { objectID: '1', title: 'Hello' },
-      { objectID: '2', title: 'React' },
-    ];
+	it('fetches stories from an API and displays them', async () => {
+		const stories = [
+			{ objectID: '1', title: 'Hello' },
+			{ objectID: '2', title: 'React' },
+		];
 
-    axios.get.mockImplementationOnce(() =>
-      Promise.resolve({ data: { hits: stories } })
-    );
+		axios.get.mockImplementationOnce(() =>
+			Promise.resolve({ data: { hits: stories } })
+		);
 
-    render(<App />);
+		render(<App />);
 
-    await userEvent.click(screen.getByRole('button'));
+		await userEvent.click(screen.getByRole('button'));
 
-    const items = await screen.findAllByRole('listitem');
+		const items = await screen.findAllByRole('listitem');
 
-    expect(items).toHaveLength(2);
-  });
+		expect(items).toHaveLength(2);
+	});
 });
 
 describe('App', () => {
+	it('fetches stories from an API and fails', async () => {
 
+		axios.get.mockImplementationOnce(() =>
+			Promise.reject(new Error())
+		);
 
-  it('fetches stories from an API and fails', async () => {
+		render(<App />);
 
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error())
-    );
+		await userEvent.click(screen.getByRole('button'));
 
-    render(<App />);
+		const message = await screen.findByText(/Something went wrong/);
 
-    await userEvent.click(screen.getByRole('button'));
+		expect(message).toBeInTheDocument();
+	});
+});
 
-    const message = await screen.findByText(/Something went wrong/);
+describe('App', () => {
+	it('fetches stories from an API and displays them', async () => {
+		const stories = [
+			{ objectID: '1', title: 'Hello' },
+			{ objectID: '2', title: 'React' },
+		];
 
-    expect(message).toBeInTheDocument();
-  });
+		const promise = Promise.resolve({ data: { hits: stories } });
+
+		axios.get.mockImplementationOnce(() => promise);
+
+		render(<App />);
+
+		await userEvent.click(screen.getByRole('button'));
+
+		waitFor(() => promise);
+
+		expect(screen.getAllByRole('listitem')).toHaveLength(2);
+	});
 });
