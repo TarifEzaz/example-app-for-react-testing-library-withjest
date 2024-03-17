976f0f9b7fb687d6ecbf6dabeb0483e44a57b054
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-17 16:21:48 +0600

    chore: format code

--- src/search.test.js
+++ src/search.test.js
@@ -5,53 +5,46 @@ import userEvent from '@testing-library/user-event';
 import App, { Search } from './Search';
 
 describe('Search', () => {
-  it('renders Search component', () => {
-    render(<App />);
+	it('renders Search component', () => {
+		render(<App />);
 
-    expect(screen.getByText('Search:')).toBeInTheDocument();
+		expect(screen.getByText('Search:')).toBeInTheDocument();
 
-//    expect(screen.getByText(/Search/)).toBeInTheDocument();
-
-    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-  });
+		expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+	});
 });
 
 describe('Search', () => {
-    it('calls the onChange callback handler', () => {
-      // Jest
-      // const onChange = jest.fn();
-      // Vitest
-      const onChange = jest.fn();
-  
-      render(
-        <Search value="" onChange={onChange}>
-          Search:
-        </Search>
-      );
-  
-      fireEvent.change(screen.getByRole('textbox'), {
-        target: { value: 'JavaScript' },
-      });
-  
-      expect(onChange).toHaveBeenCalledTimes(1);
-    });
+	it('calls the onChange callback handler', () => {
+		const onChange = jest.fn();
+
+		render(
+			<Search value="" onChange={onChange}>
+				Search:
+			</Search>
+		);
+
+		fireEvent.change(screen.getByRole('textbox'), {
+			target: { value: 'JavaScript' },
+		});
+
+		expect(onChange).toHaveBeenCalledTimes(1);
+	});
 });
 
 describe('Search', () => {
-    it('calls the onChange callback handler', async () => {
-      // Jest
-      // const onChange = jest.fn();
-      // Vitest
-      const onChange = jest.fn();
-  
-      render(
-        <Search value="" onChange={onChange}>
-          Search:
-        </Search>
-      );
-  
-      await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
-  
-      expect(onChange).toHaveBeenCalledTimes(10);
-    });
+	it('calls the onChange callback handler', async () => {
+		
+		const onChange = jest.fn();
+
+		render(
+			<Search value="" onChange={onChange}>
+				Search:
+			</Search>
+		);
+
+		await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+
+		expect(onChange).toHaveBeenCalledTimes(10);
+	});
   });
