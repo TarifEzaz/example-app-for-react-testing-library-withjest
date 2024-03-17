1a6017ed857d7e8c0d10f2a4549702c2ccef0811
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-17 16:22:27 +0600

    chore: format code

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('true is truthy and false is falsy', () => {
-  it('true is truthy', () => {
-    expect(true).toBe(true);
-  });
+	it('true is truthy', () => {
+		expect(true).toBe(true);
+	});
 
-  it('false is falsy', () => {
-    expect(false).toBe(false);
-  });
+	it('false is falsy', () => {
+		expect(false).toBe(false);
+	});
 });
 
 describe('App', () => {
-  it('renders App component', () => {
-    render(<App />);
+	it('renders App component', () => {
+		render(<App />);
 
-    screen.debug();
-  });
+		screen.debug();
+	});
 });
