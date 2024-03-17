1ede67ceac6f5e2b392c2b41d30823dfa6f09017
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-17 16:13:06 +0600

    chore: format code

--- src/Algolia.js
+++ src/Algolia.js
@@ -4,38 +4,38 @@ import axios from 'axios';
 const URL = 'http://hn.algolia.com/api/v1/search';
 
 function App() {
-  const [stories, setStories] = React.useState([]);
-  const [error, setError] = React.useState(null);
+	const [stories, setStories] = React.useState([]);
+	const [error, setError] = React.useState(null);
 
-  async function handleFetch(event) {
-    let result;
+	async function handleFetch(event) {
+		let result;
 
-    try {
-      result = await axios.get(`${URL}?query=React`);
+		try {
+			result = await axios.get(`${URL}?query=React`);
 
-      setStories(result.data.hits);
-    } catch (error) {
-      setError(error);
-    }
-  }
+			setStories(result.data.hits);
+		} catch (error) {
+			setError(error);
+		}
+	}
 
-  return (
-    <div>
-      <button type="button" onClick={handleFetch}>
-        Fetch Stories
-      </button>
+	return (
+		<div>
+			<button type="button" onClick={handleFetch}>
+				Fetch Stories
+			</button>
 
-      {error && <span>Something went wrong ...</span>}
+			{error && <span>Something went wrong ...</span>}
 
-      <ul>
-        {stories.map((story) => (
-          <li key={story.objectID}>
-            <a href={story.url}>{story.title}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+			<ul>
+			{stories.map((story) => (
+				<li key={story.objectID}>
+				<a href={story.url}>{story.title}</a>
+				</li>
+			))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
